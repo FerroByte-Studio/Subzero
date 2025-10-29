fb582d43aa0b771b2913964c3c3c167b4fa1c9d3
Max Kaiser <max@kaiser.family>
2025-10-29 22:24:47 +0100

    Started implementing Stats Component

---
+++ Source/Subzero/Private/Components/SZ_StatsComponent.cpp
@@ -0,0 +1,59 @@
+﻿// Copyright FerroByte Studio
+
+
+#include "Components/SZ_StatsComponent.h"
+
+
+// Sets default values for this component's properties
+USZ_StatsComponent::USZ_StatsComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void USZ_StatsComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void USZ_StatsComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                       FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+bool USZ_StatsComponent::DecreaseThirst(float Amount)
+{
+	if (Amount <= 0.f || Thirst < Amount)
+	{
+		return false;
+	}
+
+	Thirst -= Amount;
+	return true;
+}
+
+bool USZ_StatsComponent::DecreaseHunger(float Amount)
+{
+	if (Amount <= 0.f || Hunger < Amount)
+	{
+		return false;
+	}
+
+	Hunger -= Amount;
+	return true;
+}
+
+
---
+++ Source/Subzero/Public/Components/SZ_StatsComponent.h
@@ -0,0 +1,70 @@
+﻿// Copyright FerroByte Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SZ_StatsComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SUBZERO_API USZ_StatsComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USZ_StatsComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	/***************** Getters *******************/
+	UFUNCTION(BlueprintCallable, Category="Subzero|Stats")
+	float GetHealth() { return Health; }
+	
+	UFUNCTION(BlueprintCallable, Category="Subzero|Stats")
+	float GetMaxHealth() { return MaxHealth; }
+	
+	UFUNCTION(BlueprintCallable, Category="Subzero|Stats")
+	float GetHunger() { return Hunger; }
+	
+	UFUNCTION(BlueprintCallable, Category="Subzero|Stats")
+	float GetMaxHunger() { return MaxHunger; }
+	
+	UFUNCTION(BlueprintCallable, Category="Subzero|Stats")
+	float GetThirst() { return Thirst; }
+	
+	UFUNCTION(BlueprintCallable, Category="Subzero|Stats")
+	float GetMaxThirst() { return MaxThirst; }
+
+	UFUNCTION(BlueprintCallable, Category="Subzero|Stats")
+	bool DecreaseThirst(float Amount);
+	UFUNCTION(BlueprintCallable, Category="Subzero|Stats")
+	bool DecreaseHunger(float Amount);
+	
+
+private:
+	UPROPERTY(EditAnywhere, Category="Subzero|Stats")
+	float Health = 100.f;
+
+	UPROPERTY(EditAnywhere, Category="Subzero|Stats")
+	float MaxHealth = 100.f;
+
+	UPROPERTY(EditAnywhere, Category="Subzero|Stats")
+	float Hunger = 100.f;
+
+	UPROPERTY(EditAnywhere, Category="Subzero|Stats")
+	float MaxHunger = 100.f;
+
+	UPROPERTY(EditAnywhere, Category="Subzero|Stats")
+	float Thirst = 100.f;
+
+	UPROPERTY(EditAnywhere, Category="Subzero|Stats")
+	float MaxThirst = 100.f;
+};
